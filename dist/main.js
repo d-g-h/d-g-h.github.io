89621f1cb595a857b92c13f07267c888b9e9c26a
Dave Hall <daveghall@gmail.com>
2017-07-31 17:32:00 -0400

    Add PWA features, increases PWA score

--- dist/main.js
+++ dist/main.js
@@ -8,11 +8,16 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__style_css___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0__style_css__);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_highlight_js_lib_highlight_js__ = __webpack_require__(11);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_highlight_js_lib_highlight_js___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_1_highlight_js_lib_highlight_js__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2_offline_plugin_runtime__ = __webpack_require__(12);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2_offline_plugin_runtime___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_2_offline_plugin_runtime__);
 
 
-__WEBPACK_IMPORTED_MODULE_1_highlight_js_lib_highlight_js__["registerLanguage"]('shell', __webpack_require__(12));
-__WEBPACK_IMPORTED_MODULE_1_highlight_js_lib_highlight_js__["registerLanguage"]('javascript', __webpack_require__(13));
+
+
+__WEBPACK_IMPORTED_MODULE_1_highlight_js_lib_highlight_js__["registerLanguage"]('shell', __webpack_require__(13));
+__WEBPACK_IMPORTED_MODULE_1_highlight_js_lib_highlight_js__["registerLanguage"]('javascript', __webpack_require__(14));
 __WEBPACK_IMPORTED_MODULE_1_highlight_js_lib_highlight_js__["initHighlightingOnLoad"]();
+__WEBPACK_IMPORTED_MODULE_2_offline_plugin_runtime__["install"]();
 
 
 /***/ }),
--- webpack.config.ts
+++ webpack.config.ts
@@ -1,5 +1,8 @@
 import * as webpack from 'webpack';
 import * as path from 'path';
+import * as OfflinePlugin from 'offline-plugin';
+import * as ManifestPlugin from 'webpack-manifest-plugin';
+
 const nodeModules = path.join(process.cwd(), 'node_modules');
 declare var __dirname;
 
@@ -32,6 +35,71 @@ const config: webpack.Configuration = {
       'name': 'vendor',
       'minChunks': (module) => module.resource && module.resource.startsWith(nodeModules),
     }),
+    new OfflinePlugin({
+      externals: [
+        './index.html',
+        './dist/vendor.js',
+        './dist/main.js',
+        './assets/images/dgh.png',
+        './assets/fonts/icomoon.svg',
+        './assets/fonts/icomoon.ttf',
+        './assets/fonts/icomoon.woff',
+        './assets/fonts/icomoon.eot',
+      ]
+    }),
+    new ManifestPlugin({
+      cache: {
+        name: 'd-g-h.co',
+        short_name: 'd-g-h',
+        display: 'minimal-ui',
+        description: 'dgh\'s thoughts',
+        start_url: '/',
+        theme_color: '#4078c0',
+        background_color: '#fff',
+        icons: [
+          {
+            'src': '/assets/images/dgh-72x72.png',
+            'sizes': '72x72',
+            'type': 'image/png'
+          },
+          {
+            'src': '/assets/images/dgh-96x96.png',
+            'sizes': '96x96',
+            'type': 'image/png'
+          },
+          {
+            'src': '/assets/images/dgh-128x128.png',
+            'sizes': '128x128',
+            'type': 'image/png'
+          },
+          {
+            'src': '/assets/images/dgh-144x144.png',
+            'sizes': '144x144',
+            'type': 'image/png'
+          },
+          {
+            'src': '/assets/images/dgh-152x152.png',
+            'sizes': '152x152',
+            'type': 'image/png'
+          },
+          {
+            'src': '/assets/images/dgh-192x192.png',
+            'sizes': '192x192',
+            'type': 'image/png'
+          },
+          {
+            'src': '/assets/images/dgh-384x384.png',
+            'sizes': '384x384',
+            'type': 'image/png'
+          },
+          {
+            'src': '/assets/images/dgh-512x512.png',
+            'sizes': '512x512',
+            'type': 'image/png'
+          }
+        ],
+      }
+    })
   ]
 };
 
