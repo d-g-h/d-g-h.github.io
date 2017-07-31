5dd1cf0af763e12cbda4d87498ba0680b154c0e3
Dave Hall <daveghall@gmail.com>
2017-07-31 00:00:09 -0400

    Vendor chunking from node_modules, update package.json, used .ts for webpack.config

---
+++ dist/main.js
@@ -0,0 +1,95 @@
+webpackJsonp([0],[
+/* 0 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__style_css__ = __webpack_require__(1);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__style_css___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0__style_css__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_highlight_js_lib_highlight_js__ = __webpack_require__(11);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_highlight_js_lib_highlight_js___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_1_highlight_js_lib_highlight_js__);
+
+
+__WEBPACK_IMPORTED_MODULE_1_highlight_js_lib_highlight_js__["registerLanguage"]('shell', __webpack_require__(12));
+__WEBPACK_IMPORTED_MODULE_1_highlight_js_lib_highlight_js__["registerLanguage"]('javascript', __webpack_require__(13));
+__WEBPACK_IMPORTED_MODULE_1_highlight_js_lib_highlight_js__["initHighlightingOnLoad"]();
+
+
+/***/ }),
+/* 1 */
+/***/ (function(module, exports, __webpack_require__) {
+
+// style-loader: Adds some css to the DOM by adding a <style> tag
+
+// load the styles
+var content = __webpack_require__(2);
+if(typeof content === 'string') content = [[module.i, content, '']];
+// Prepare cssTransformation
+var transform;
+
+var options = {}
+options.transform = transform
+// add the styles to the DOM
+var update = __webpack_require__(9)(content, options);
+if(content.locals) module.exports = content.locals;
+// Hot Module Replacement
+if(false) {
+	// When the styles change, update the <style> tags
+	if(!content.locals) {
+		module.hot.accept("!!./node_modules/css-loader/index.js!./style.css", function() {
+			var newContent = require("!!./node_modules/css-loader/index.js!./style.css");
+			if(typeof newContent === 'string') newContent = [[module.id, newContent, '']];
+			update(newContent);
+		});
+	}
+	// When the module is disposed, remove the <style> tags
+	module.hot.dispose(function() { update(); });
+}
+
+/***/ }),
+/* 2 */
+/***/ (function(module, exports, __webpack_require__) {
+
+exports = module.exports = __webpack_require__(3)(undefined);
+// imports
+
+
+// module
+exports.push([module.i, "html {\n  font-family: sans-serif;\n  -ms-text-size-adjust: 100%;\n  -webkit-text-size-adjust: 100%;\n}\nbody {\n  margin: 0;\n}\narticle,\naside,\ndetails,\nfigcaption,\nfigure,\nfooter,\nheader,\nhgroup,\nmain,\nmenu,\nnav,\nsection,\nsummary {\n  display: block;\n}\naudio,\ncanvas,\nprogress,\nvideo {\n  display: inline-block;\n  vertical-align: baseline;\n}\naudio:not([controls]) {\n  display: none;\n  height: 0;\n}\n[hidden],\ntemplate {\n  display: none;\n}\na {\n  background-color: transparent;\n}\na:active,\na:hover {\n  outline: 0;\n}\nabbr[title] {\n  border-bottom: 1px dotted;\n}\nb,\nstrong {\n  font-weight: bold;\n}\ndfn {\n  font-style: italic;\n}\nh1 {\n  font-size: 2em;\n  margin: 0.67em 0;\n}\nmark {\n  background: #ff0;\n  color: #000;\n}\nsmall {\n  font-size: 80%;\n}\nsub,\nsup {\n  font-size: 75%;\n  line-height: 0;\n  position: relative;\n  vertical-align: baseline;\n}\nsup {\n  top: -0.5em;\n}\nsub {\n  bottom: -0.25em;\n}\nimg {\n  border: 0;\n}\nsvg:not(:root) {\n  overflow: hidden;\n}\nfigure {\n  margin: 1em 40px;\n}\nhr {\n  box-sizing: content-box;\n  height: 0;\n}\npre {\n  overflow: auto;\n}\ncode,\nkbd,\npre,\nsamp {\n  font-family: monospace, monospace;\n  font-size: 1em;\n}\nbutton,\ninput,\noptgroup,\nselect,\ntextarea {\n  color: inherit;\n  font: inherit;\n  margin: 0;\n}\nbutton {\n  overflow: visible;\n}\nbutton,\nselect {\n  text-transform: none;\n}\nbutton,\nhtml input[type=\"button\"],\ninput[type=\"reset\"],\ninput[type=\"submit\"] {\n  -webkit-appearance: button;\n  cursor: pointer;\n}\nbutton[disabled],\nhtml input[disabled] {\n  cursor: default;\n}\nbutton::-moz-focus-inner,\ninput::-moz-focus-inner {\n  border: 0;\n  padding: 0;\n}\ninput {\n  line-height: normal;\n}\ninput[type=\"checkbox\"],\ninput[type=\"radio\"] {\n  box-sizing: border-box;\n  padding: 0;\n}\ninput[type=\"number\"]::-webkit-inner-spin-button,\ninput[type=\"number\"]::-webkit-outer-spin-button {\n  height: auto;\n}\ninput[type=\"search\"] {\n  -webkit-appearance: textfield;\n  box-sizing: content-box;\n}\ninput[type=\"search\"]::-webkit-search-cancel-button,\ninput[type=\"search\"]::-webkit-search-decoration {\n  -webkit-appearance: none;\n}\nfieldset {\n  border: 1px solid #c0c0c0;\n  margin: 0 2px;\n  padding: 0.35em 0.625em 0.75em;\n}\nlegend {\n  border: 0;\n  padding: 0;\n}\ntextarea {\n  overflow: auto;\n}\noptgroup {\n  font-weight: bold;\n}\ntable {\n  border-collapse: collapse;\n  border-spacing: 0;\n}\ntd,\nth {\n  padding: 0;\n}\n.hljs {\n  display: block;\n  overflow-x: auto;\n  padding: 0.5em;\n  background: #00222b;\n  color: #839496;\n  -webkit-text-size-adjust: none;\n}\n.hljs-comment,\n.diff .hljs-header,\n.hljs-doctype,\n.hljs-pi,\n.lisp .hljs-string {\n  color: #aaaaaa;\n}\n.hljs-keyword,\n.hljs-winutils,\n.method,\n.hljs-addition,\n.css .hljs-tag,\n.hljs-request,\n.hljs-status,\n.nginx .hljs-title {\n  color: #859900;\n}\n.hljs-number,\n.hljs-command,\n.hljs-string,\n.hljs-tag .hljs-value,\n.hljs-rule .hljs-value,\n.hljs-doctag,\n.tex .hljs-formula,\n.hljs-regexp,\n.hljs-hexcolor,\n.hljs-link_url {\n  color: #2aa198;\n}\n.hljs-title,\n.hljs-localvars,\n.hljs-chunk,\n.hljs-decorator,\n.hljs-built_in,\n.hljs-identifier,\n.vhdl .hljs-literal,\n.hljs-id,\n.css .hljs-function,\n.hljs-name {\n  color: #268bd2;\n}\n.hljs-attribute,\n.hljs-variable,\n.lisp .hljs-body,\n.smalltalk .hljs-number,\n.hljs-constant,\n.hljs-class .hljs-title,\n.hljs-parent,\n.hljs-type,\n.hljs-link_reference {\n  color: #b58900;\n}\n.hljs-preprocessor,\n.hljs-preprocessor .hljs-keyword,\n.hljs-pragma,\n.hljs-shebang,\n.hljs-symbol,\n.hljs-symbol .hljs-string,\n.diff .hljs-change,\n.hljs-special,\n.hljs-attr_selector,\n.hljs-subst,\n.hljs-cdata,\n.css .hljs-pseudo,\n.hljs-header {\n  color: #cb4b16;\n}\n.hljs-deletion,\n.hljs-important {\n  color: #dc322f;\n}\n.hljs-link_label {\n  color: #6c71c4;\n}\n.tex .hljs-formula {\n  background: #073642;\n}\nhtml {\n  box-sizing: border-box;\n}\n*,\n*:before,\n*:after {\n  box-sizing: inherit;\n}\n@font-face {\n  font-family: 'icomoon';\n  src: url(" + __webpack_require__(4) + ");\n  src: url(" + __webpack_require__(5) + "?#iefix-rsav07) format(\"embedded-opentype\"),\n    url(" + __webpack_require__(6) + ") format(\"woff\"),\n    url(" + __webpack_require__(7) + ") format(\"truetype\"),\n    url(" + __webpack_require__(8) + "#icomoon) format(\"svg\");\n  font-weight: normal;\n  font-style: normal;\n}\n.icon-mark-github:before,\n.icon-file-text:before {\n  font-family: 'icomoon';\n  speak: none;\n  font-style: normal;\n  font-weight: normal;\n  font-variant: normal;\n  text-transform: none;\n  line-height: 1;\n  -webkit-font-smoothing: antialiased;\n  -moz-osx-font-smoothing: grayscale;\n}\n.icon-mark-github:before {\n  content: \"\\E600\";\n}\n.icon-file-text:before {\n  content: \"\\E601\";\n}\nbody {\n  color: #333;\n  font: 13px / 1.4 system, -apple-system, \".SFNSDisplay-Regular\",\n    \"Helvetica Neue\", Helvetica, arial, nimbussansl, liberationsans, freesans,\n    clean, sans-serif, \"Segoe UI Emoji\", \"Segoe UI Symbol\";\n}\na {\n  color: #4078c0;\n  text-decoration: none;\n}\na:hover {\n  text-decoration: underline;\n}\na span {\n  font-size: 8px;\n  padding-left: .25em;\n}\nh1 {\n  text-align: center;\n}\nh2 {\n  margin-bottom: 0;\n}\nh3,\nh4,\nh5,\nh6 {\n  color: #666;\n}\nli {\n  list-style: none;\n}\np {\n  margin: .5em 0;\n}\ntime {\n  color: #999;\n  font: 13px / 1 system, -apple-system, \".SFNSDisplay-Regular\", \"Helvetica Neue\",\n    Helvetica, arial, nimbussansl, liberationsans, freesans, clean, sans-serif,\n    \"Segoe UI Emoji\", \"Segoe UI Symbol\";\n}\n.logo {\n  background-image: url(\"/assets/images/dgh.png\");\n  background-size: contain;\n  border-radius: 50%;\n  display: block;\n  height: 3em;\n  margin: 0 auto;\n  width: 3em;\n}\nheader {\n  padding-top: 1rem;\n}\nnav ul {\n  display: -webkit-box;\n  display: -webkit-flex;\n  display: -ms-flexbox;\n  display: flex;\n  -webkit-flex-wrap: wrap;\n  -ms-flex-wrap: wrap;\n  flex-wrap: wrap;\n  -webkit-box-pack: center;\n  -webkit-justify-content: center;\n  -ms-flex-pack: center;\n  justify-content: center;\n  margin: 0;\n  padding: 0;\n}\nnav ul li {\n  -webkit-align-self: flex-end;\n  -ms-flex-item-align: end;\n  align-self: flex-end;\n  padding: 1em;\n  text-align: center;\n}\nnav ul a {\n  font-size: 12px;\n}\n.contribute {\n  font-size: 10px;\n}\n.icon-mark-github:before {\n  font-size: 24px;\n  padding: 4px;\n}\n.icon-file-text:before {\n  font-size: 24px;\n  padding: 4px;\n}\n.container {\n  border-top: 1px solid #999;\n  font-size: 16px;\n  margin: 0 auto;\n  max-width: 800px;\n  padding: .5em;\n}\nvideo {\n  display: block;\n  margin: 0 auto;\n  max-width: 100%;\n}\n.job-title,\n.contact-info {\n  color: #4078c0;\n  text-align: center;\n}\n.contact-info span {\n  font-size: 50%;\n}\n.job-time {\n  color: #999;\n  font-size: 10.66667px;\n}\n.resume.container {\n  border: 0;\n}\n.resume.container h2,\n.resume.container h3,\n.resume.container h4,\n.resume.container h5,\n.resume.container p {\n  margin: 0;\n}\n.resume.container h2,\n.resume.container h3,\n.resume.container h4,\n.resume.container h5 {\n  color: #333;\n}\n.resume.container p {\n  color: #666;\n}\n.resume.container p span {\n  font-size: 75%;\n}\n.resume.container a {\n  display: block;\n}\narticle {\n  margin-top: 1em;\n}\n@media print {\n  .resume {\n    font-size: 85%;\n  }\n}\n", ""]);
+
+// exports
+
+
+/***/ }),
+/* 3 */,
+/* 4 */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__.p + "75cf10f313532a120f287de6a31e64bb.eot";
+
+/***/ }),
+/* 5 */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__.p + "75cf10f313532a120f287de6a31e64bb.eot";
+
+/***/ }),
+/* 6 */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__.p + "5f49880e43263c57f009240776e181e0.woff";
+
+/***/ }),
+/* 7 */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__.p + "00958aa80c387f15ac23398ce58da5e6.ttf";
+
+/***/ }),
+/* 8 */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__.p + "f3efa91a2cf7b68aee70aa70dc8eb3db.svg";
+
+/***/ })
+],[0]);
