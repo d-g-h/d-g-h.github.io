f1a01a9b4fff35f86170636b0a606200f7a33896
Dave Hall <daveghall@gmail.com>
2017-07-31 17:32:00 -0400

    Add PWA features, increases PWA score

--- dist/vendor.js
+++ dist/vendor.js
@@ -1517,6 +1517,98 @@ https://highlightjs.org/
 /* 12 */
 /***/ (function(module, exports) {
 
+var appCacheIframe;
+
+function hasSW() {
+  return 'serviceWorker' in navigator &&
+    // This is how I block Chrome 40 and detect Chrome 41, because first has
+    // bugs with history.pustState and/or hashchange
+    (window.fetch || 'imageRendering' in document.documentElement.style) &&
+    (window.location.protocol === 'https:' || window.location.hostname === 'localhost' || window.location.hostname.indexOf('127.') === 0)
+}
+
+function install(options) {
+  options || (options = {});
+
+  
+    if (hasSW()) {
+      var registration = navigator.serviceWorker
+        .register(
+          "sw.js"
+          
+        );
+
+      
+
+      return;
+    }
+  
+
+  
+    if (window.applicationCache) {
+      var directory = "appcache/";
+      var name = "manifest";
+
+      var doLoad = function() {
+        var page = directory + name + '.html';
+        var iframe = document.createElement('iframe');
+
+        
+
+        iframe.src = page;
+        iframe.style.display = 'none';
+
+        appCacheIframe = iframe;
+        document.body.appendChild(iframe);
+      };
+
+      if (document.readyState === 'complete') {
+        setTimeout(doLoad);
+      } else {
+        window.addEventListener('load', doLoad);
+      }
+
+      return;
+    }
+  
+}
+
+function applyUpdate(callback, errback) {
+  
+
+  
+}
+
+function update() {
+  
+    if (hasSW()) {
+      navigator.serviceWorker.getRegistration().then(function(registration) {
+        if (!registration) return;
+        return registration.update();
+      });
+    }
+  
+
+  
+    if (appCacheIframe) {
+      try {
+        appCacheIframe.contentWindow.applicationCache.update();
+      } catch (e) {}
+    }
+  
+}
+
+
+
+exports.install = install;
+exports.applyUpdate = applyUpdate;
+exports.update = update;
+
+
+/***/ }),
+/* 13 */
+/***/ (function(module, exports) {
+
 module.exports = function(hljs) {
   return {
     aliases: ['console'],
@@ -1533,7 +1625,7 @@ module.exports = function(hljs) {
 };
 
 /***/ }),
-/* 13 */
+/* 14 */
 /***/ (function(module, exports) {
 
 module.exports = function(hljs) {
