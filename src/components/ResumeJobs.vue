3efc52c71499b4f7a95d3c2f9e4abee3c0b7ccb6
Dave Hall <dave.hall@ibm.com>
2022-05-19 17:08:23 -0400

    Rename components for native HTML elements, update curr job

--- src/components/ResumeJobs.vue
+++ src/components/ResumeJobs.vue
@@ -8,105 +8,109 @@
     </div>
     <div>
       <ul>
-        <Labor
+        <LaborJob
           title="Software Developer"
           location="New York, NY"
           company="Kyndryl"
           time="November 2021 - Present"
         >
-        </Labor>
+          <ul>
+            <DescriptionJob
+              description="Develop single page applications using Vue, and services using TypeScript, Node.js"
+            />
+          </ul>
+        </LaborJob>
 
-        <Labor
+        <LaborJob
           title="Software Engineer"
           location="New York, NY"
           company="IBM"
           time="August 2018 - November 2021"
         >
           <ul>
-            <Description
+            <DescriptionJob
               description="Deliver HTML, JavaScript, & CSS to the client with Vue, and TypeScript"
             />
-            <Description
+            <DescriptionJob
               description="Create maintainable code with Functional programming and Test Driven Development"
             />
-            <Description
-              description="Collaborate using Agile Development Methodologies to construct software"
+            <DescriptionJob
+              description="Collaborate using agile development methodologies to construct software"
             />
           </ul>
-        </Labor>
+        </LaborJob>
 
-        <Labor
+        <LaborJob
           title="Software Engineer"
           location="New York, NY"
           company="Hook & Loop at Infor"
           time="February 2016 - August 2018"
         >
           <ul>
-            <Description
+            <DescriptionJob
               description="Architect, and deliver, compiled HTML, JavaScript, & CSS via Angular, Webpack"
             />
           </ul>
-        </Labor>
+        </LaborJob>
 
-        <Labor
+        <LaborJob
           title="Freelance Full Stack Web Developer"
           location="New York, NY"
           company="d-g-h.co"
           time="January 2009 - Present"
         >
           <ul>
-            <Description
+            <DescriptionJob
               description="Architect and plan coding solutions for responsive websites"
             />
           </ul>
-        </Labor>
+        </LaborJob>
 
-        <Labor
+        <LaborJob
           title="Lead Developer"
           location="New York, NY"
           company="Radish Lab"
           time="December 2013 - July 2015"
         >
           <ul>
-            <Description
+            <DescriptionJob
               description="Create, and manage developer operations"
             />
-            <Description
-              description="Scope, manage, architect projects"
-            />
-            <Description
+            <DescriptionJob description="Scope, manage, architect projects" />
+            <DescriptionJob
               description="Build sites, and RESTful APIs using WordPress, Node.js, ExpressJS, and AngularJS"
             />
           </ul>
-        </Labor>
+        </LaborJob>
 
-        <Labor
+        <LaborJob
           title="Web Developer"
           location="New York, NY"
           company="Graph Paper Press"
           time="August 2012 - April 2014"
         >
           <ul>
-            <Description
+            <DescriptionJob
               description="Create and maintain environments for seamless developer operations"
             />
-            <Description
+            <DescriptionJob
               description="Offer consultation about various design, development, and business practices"
             />
-            <Description
-              description="Develop customized WordPress themes"
-            />
-            <Description
+            <DescriptionJob description="Develop customized WordPress themes" />
+            <DescriptionJob
               description="Triage and respond to theme related bugs"
             />
           </ul>
-        </Labor>
+        </LaborJob>
       </ul>
     </div>
     <div>
       <div class="title">Education</div>
       <ul>
-        <li>Brooklyn College, Information Systems, MS Candidate (completed all course requirements)</li>
+        <li>
+          Brooklyn College, Information Systems, MS Candidate (completed all
+          course requirements)
+        </li>
         <li>City College of New York, General Psychology, MA 2008</li>
         <li>Binghamton University, Psychology, BA 2005</li>
       </ul>
@@ -115,14 +119,14 @@
 </template>
 
 <script>
-import Labor from "./Labor.vue";
-import Description from "./Description.vue";
+import LaborJob from "./LaborJob.vue";
+import DescriptionJob from "./DescriptionJob.vue";
 
 export default {
-  name: "Resume",
+  name: "ResumeJobs",
   components: {
-    Labor,
-    Description,
+    LaborJob,
+    DescriptionJob,
   },
 };
 </script>
